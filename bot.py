178bd926dbfda4e44ff7fd9f2931df4cb180d940
Akhil Narang <akhil@akhilnarang.me>
2018-03-03 00:41:38 +0530

    lulzk
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- bot.py
+++ bot.py
@@ -722,9 +722,9 @@ dispatcher.add_handler(CommandHandler('leave', leave_game))
 dispatcher.add_handler(CommandHandler(['kick', 'boot'], kick_player))
 dispatcher.add_handler(CommandHandler('open', open_game))
 dispatcher.add_handler(CommandHandler('close', close_game))
-dispatcher.add_handler(CommandHandler('enable_translations',
+#dispatcher.add_handler(CommandHandler('enable_translations',
                                       enable_translations))
-dispatcher.add_handler(CommandHandler('disable_translations',
+#dispatcher.add_handler(CommandHandler('disable_translations',
                                       disable_translations))
 dispatcher.add_handler(CommandHandler('skip', skip_player))
 dispatcher.add_handler(CommandHandler('notify_me', notify_me))
